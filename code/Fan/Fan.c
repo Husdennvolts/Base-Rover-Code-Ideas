9143700c255dd5982039e72daac8defd6a5dda5a
Husdennvolts <65894300+Husdennvolts@users.noreply.github.com>
2025-10-08 13:55:10 -0400

    changes made to fan files

--- code/Fan/Fan.c
+++ code/Fan/Fan.c
@@ -1,10 +1,17 @@
 #include "Fan.h"
 #include "Init/Init.h"
 
+/*----------------------------functions processed when message is received-----------------------------------------*/
 void processFanMsg(canMsg_t msg)
 {
     if(getDevice() == FANBOARD)
     {
         //use mssage data to control the Fans
     }
+}
+
+/*------------------------------------functions processed every tick-----------------------------------------------*/
+float tempRunningAvg(float tempSense1, float tempSense2)
+{
+    //pass the temp sensor readings in and create a running average to 
 }
--- code/Fan/Fan.h
+++ code/Fan/Fan.h
@@ -1,3 +1,12 @@
 #include "definitions.h"
 
+typedef struct
+{
+    float tempAvg1;
+    float tempAvg2;
+    int samples;
+    const int maxSamples;// = 10;
+}runningAvg_t;
+
 void processFanMsg(canMsg_t);
+float tempRunningAvg(float, float);
